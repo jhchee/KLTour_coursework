474178ba1916d12ca59f5549397422af9b928475
jhchee <cheehong05090@gmail.com>
2018-03-30 12:41:06 +0800

    add login overlay

---
+++ scripting/loginform.js
@@ -0,0 +1,9 @@
+function displayform (){
+    var i = document.getElementById('login');
+    i.style.display="block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
