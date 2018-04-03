0fdb62c8c392662bd1d669f06d4670a18f49bfef
jhchee <cheehong05090@gmail.com>
2018-04-03 12:26:36 +0800

    query

--- queries.sql
+++ queries.sql
@@ -9,8 +9,10 @@ INSERT INTO main_order(Username, Package_ID, Total_price) VALUES ('Chee',1,0.00)
 
 -- view the unpaid order
 UPDATE main_order
-SET Paid/Unpaid = 1
+SET Paid = 1
 WHERE Username = "Chee";
 
 -- view the paid order
 SELECT * FROM main_order WHERE Username = '' AND Paid/unpaid = 1
+
+-- view the 
