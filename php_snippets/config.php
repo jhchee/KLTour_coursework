003b9f391104e4ce175d1b71df381e3acb5a5a18
jhchee <cheehong05090@gmail.com>
2018-04-17 18:57:24 +0800

    delete comment

--- php_snippets/config.php
+++ php_snippets/config.php
@@ -1,19 +1,3 @@
-<!-- $servername = 'localhost';
-$username = 'username';
-$password = '';
-$dbName = 'kltour';
-
-try {
-    // Create database connection
-    $db = new mysqli($servername, $username, $password, $dbName);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "hello world";
-}
-catch(PDOException $e) {
-    // If connection error, return error message
-    echo "Error: " . $e->getMessage();
-} -->
-
 <?php
     $host = "localhost";
     $userName = "root";
