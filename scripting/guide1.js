cbe19927d00707a93744ebe58a576b3f6d13103f
jhchee <cheehong05090@gmail.com>
2018-04-25 16:41:32 +0800

    guide images

---
+++ scripting/guide1.js
@@ -0,0 +1,178 @@
+$(document).ready(function(){
+    $('#b1-1').click(function(){
+        $('#b1-1').fadeOut();
+        $("#p1, #b1-2").fadeIn('slow',function(){
+            document.getElementById("p1").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b1-2').click(function(){
+        $('#b1-1').fadeIn();
+        $("#p1, #b1-2").fadeOut('slow',function(){
+            document.getElementById("p1").style.display="none";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b2-1').click(function(){
+        $('#b2-1').fadeOut();
+        $("#p2, #b2-2").fadeIn('slow',function(){
+            document.getElementById("p2").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b2-2').click(function(){
+        $('#b2-1').fadeIn();
+        $("#p2, #b2-2").fadeOut('slow',function(){
+            document.getElementById("p2").style.display="none";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b3-1').click(function(){
+        $('#b3-1').fadeOut();
+        $("#p3, #b3-2").fadeIn('slow',function(){
+            document.getElementById("p3").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b3-2').click(function(){
+        $('#b3-1').fadeIn();
+        $("#p3, #b3-2").fadeOut('slow',function(){
+            document.getElementById("p3").style.display="none";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b4-1').click(function(){
+        $('#b4-1').fadeOut();
+        $("#p4, #b4-2").fadeIn('slow',function(){
+            document.getElementById("p4").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b4-2').click(function(){
+        $('#b4-1').fadeIn();
+        $("#p4, #b4-2").fadeOut('slow',function(){
+            document.getElementById("p4").style.display="none";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b5-1').click(function(){
+        $('#b5-1').fadeOut();
+        $("#p5, #b5-2").fadeIn('slow',function(){
+            document.getElementById("p5").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b5-2').click(function(){
+        $('#b5-1').fadeIn();
+        $("#p5, #b5-2").fadeOut('slow',function(){
+            document.getElementById("p5").style.display="none";
+        });
+    });
+});
+$(document).ready(function(){
+    $('#b6-1').click(function(){
+        $('#b6-1').fadeOut();
+        $("#p6, #b6-2").fadeIn('slow',function(){
+            document.getElementById("p6").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b6-2').click(function(){
+        $('#b6-1').fadeIn();
+        $("#p6, #b6-2").fadeOut('slow',function(){
+            document.getElementById("p6").style.display="none";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b7-1').click(function(){
+        $('#b7-1').fadeOut();
+        $("#p7, #b7-2").fadeIn('slow',function(){
+            document.getElementById("p7").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b7-2').click(function(){
+        $('#b7-1').fadeIn();
+        $("#p7, #b7-2").fadeOut('slow',function(){
+            document.getElementById("p7").style.display="none";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b8-1').click(function(){
+        $('#b8-1').fadeOut();
+        $("#p8, #b8-2").fadeIn('slow',function(){
+            document.getElementById("p8").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b8-2').click(function(){
+        $('#b8-1').fadeIn();
+        $("#p8, #b8-2").fadeOut('slow',function(){
+            document.getElementById("p8").style.display="none";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b9-1').click(function(){
+        $('#b9-1').fadeOut();
+        $("#p9, #b9-2").fadeIn('slow',function(){
+            document.getElementById("p9").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b9-2').click(function(){
+        $('#b9-1').fadeIn();
+        $("#p9, #b9-2").fadeOut('slow',function(){
+            document.getElementById("p9").style.display="none";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b10-1').click(function(){
+        $('#b10-1').fadeOut();
+        $("#p10, #b10-2").fadeIn('slow',function(){
+            document.getElementById("p10").style.display="block";
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#b10-2').click(function(){
+        $('#b10-1').fadeIn();
+        $("#p10, #b10-2").fadeOut('slow',function(){
+            document.getElementById("p10").style.display="none";
+        });
+    });
+});
